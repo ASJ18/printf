cccb39c1d9bb49266e0667a35e5a211d58c25b84
Abdulwahab <wahade2004@gmail.com>
2022-10-16 15:06:14 -0700

    trial and errors

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-AUTHORS: ADEDIRAN ABDULWAHAB ADESOJI
-https://github.com/ASJ18
-SOYEMI DAVID OLASUBOMI
-https://github.com/Dking2244
+AUTHORS: ADEDIRAN ABDULWAHAB https://github.com/ASJ18
+	SHOYEMI DAVID https://github.com/Dking2244
--- main.h
+++ main.h
@@ -12,8 +12,8 @@
  */
 typedef struct format
 {
-	char *sign;
-	int (*fun)(va_list);
+	char *signal;
+	int (*lava)(va_list);
 } format_t;
 
 int _printf(const char *format, ...);
@@ -24,19 +24,5 @@ int _putstring(char);
 int _strlen(char *s);
 int print_number(va_list);
 int checker(const char *format, format_t symbols[], va_list str);
-int print_binary(va_list);
-char *rev_string(char *s);
-unsigned int base_len(unsigned int num, int base);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int unsigned_integer(va_list);
-int print_unsigned_number(unsigned int n);
-int print_octal(va_list);
-int print_hex(va_list);
-int print_Hex(va_list);
-int hex_check(int num, char x);
-int print_reversed(va_list);
-int rot13(va_list);
-int print_address(va_list);
-char *convert(unsigned int num, int base);
 
 #endif
